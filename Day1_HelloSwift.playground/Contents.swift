1db1f3c6fa46ffc45da619722ed5ccfffc4688e9
khennardy <khennardysutanto9009@gmail.com>
2025-04-24 14:25:41 +0900

    Initital commit: Day 1 Hello Swift

---
+++ Day1_HelloSwift.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+print("Hello, Swift World!")
+
+//try variable
+var name = "Khennardy"
+print("Hi,\(name)! Welcome to Swift learning.")
+
+// constants
+let birthYear = 1994
+print("You were born in \(birthYear)")
+
+// math operations
+let weight = 73
+let height = 1.75
+let bmi = Double(weight) / (height * height)
+print("Your BMI is \(bmi)")
+
+let goal = "Become a senior iOs developer"
+print("My goal is to: \(goal)")
