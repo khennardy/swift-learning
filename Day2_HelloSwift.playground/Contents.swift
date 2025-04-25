2658e7108ebb07883e8cc6db58f3bd0606187c73
khennardy <khennardysutanto9009@gmail.com>
2025-04-25 12:36:58 +0900

    Initial commit for 2nd day of learning Swift

---
+++ Day2_HelloSwift.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let name = "Khennardy"
+let age = 30
+let city = "Osaka"
+let learningSwift = true
+let isLearning = learningSwift ? "Yes" : "No"
+print("Hi, I am \(name), I am \(age) years old, living in \(city). Am I learning Swift? \(isLearning)")
