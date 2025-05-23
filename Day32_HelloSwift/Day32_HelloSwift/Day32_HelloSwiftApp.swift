ce3d7d0ccf76989a688e358866d79fc194abbdef
khennardy <khennardysutanto9009@gmail.com>
2025-05-23 17:06:17 +0900

    initial commit of learning swift day 32: editable profile form

---
+++ Day32_HelloSwift/Day32_HelloSwift/Day32_HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day32_HelloSwiftApp.swift
+//  Day32_HelloSwift
+//
+//  Created by Khennardy Sutanto on 2025/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Day32_HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProfileCardView()
+        }
+    }
+}
---
+++ Day32_HelloSwift/Day32_HelloSwiftTests/Day32_HelloSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  Day32_HelloSwiftTests.swift
+//  Day32_HelloSwiftTests
+//
+//  Created by Khennardy Sutanto on 2025/05/23.
+//
+
+import Testing
+
+struct Day32_HelloSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
