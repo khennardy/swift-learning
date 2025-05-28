c4c5633a40f8feefa64992ed7d86fae262c60ff0
khennardy <khennardysutanto9009@gmail.com>
2025-05-28 11:14:11 +0900

    initial commit of learning swift day 34. @State list, @ViewBuilder, .actionBUttonStyle(), NavigationStack

---
+++ Day34_HelloSwift/Day34_HelloSwift/Day34_HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day34_HelloSwiftApp.swift
+//  Day34_HelloSwift
+//
+//  Created by Khennardy Sutanto on 2025/05/27.
+//
+
+import SwiftUI
+
+@main
+struct Day34_HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProfileCardView()
+        }
+    }
+}
---
+++ Day34_HelloSwift/Day34_HelloSwiftTests/Day34_HelloSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  Day34_HelloSwiftTests.swift
+//  Day34_HelloSwiftTests
+//
+//  Created by Khennardy Sutanto on 2025/05/27.
+//
+
+import Testing
+
+struct Day34_HelloSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
